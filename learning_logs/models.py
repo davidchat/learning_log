c0c526792c81556bad0c6855117a846226ca8fd6
David Chatelain <dchatela@tulane.edu>
2020-09-28 11:16:52 -0600

    Change to burnt orange color scheme. Add Learning Notepad brand logo.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
-# Create your models here.
+
 class Topic(models.Model):
 	"""A topic the user is learning about."""
 	text = models.CharField(max_length=200)
