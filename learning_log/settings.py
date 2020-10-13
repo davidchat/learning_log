cab568737aa68712938037c259b69ed160d030e8
David Chatelain <dchatela@tulane.edu>
2020-10-13 12:31:51 -0600

    Update settings static files

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -139,13 +139,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
