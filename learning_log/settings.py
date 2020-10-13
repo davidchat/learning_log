9dc523a62f81d2b606416033a0985f02e7415051
David Chatelain <dchatela@tulane.edu>
2020-10-13 13:00:00 -0600

    Change static root setting

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -140,12 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
